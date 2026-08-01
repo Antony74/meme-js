cc1ce3077fcb8a47072c832394fe2df80def063a
Antony74 <akb@akb.me.uk>
2026-08-01 10:43:28 +0100

    Simplify

--- memeWithP5.js
+++ memeWithP5.js
@@ -1,12 +1,10 @@
-const main = async () => {
-  await import("./p5.min.js"); // https://cdn.jsdelivr.net/npm/p5@1.11.13/lib/p5.min.js
+const promise = import("./p5.min.js"); // https://cdn.jsdelivr.net/npm/p5@1.11.13/lib/p5.min.js
 
+promise.then(() => {
   new p5((p) => {
     p.setup = () => {
       p.createCanvas(100, 100);
       p.background(200);
     };
   });
-};
-
-main();
+});
