52f3e2266ac0d9a4f7f0d50e303ae53e574e4e5f
Antony74 <akb@akb.me.uk>
2026-08-03 23:10:38 +0100

    Revert "Oh look, I can include JavaScript from JavaScript as a one liner.  Probably shouldn't, though"
    This reverts commit 99d9f678f5a839af0919ff72d1334aca9f57d060.

--- memeWithP5.js
+++ memeWithP5.js
@@ -1,22 +1,14 @@
-const sketch = () => {
-  new p5((p) => {
-    p.setup = async () => {
-      const img = await p.loadImage("cheezburger.jpg"); // https://share.google/Qg3HnLI440Cy0Q7ZV
-      p.createCanvas(237, 389);
-      p.image(img, 0, 0);
-      p.fill(255);
-      p.textFont("Impact");
-      p.textSize(29);
-      p.text("I CAN HAS", 60, 36);
-      p.text("CHEEZBURGER?", 30, 70);
-    };
-  });
-};
+new p5((p) => {
+  let img;
 
-document.head.appendChild(
-  Object.assign(document.createElement("script"), {
-    src: "./p5.min.js",
-    onreadystatechange: sketch,
-    onload: sketch,
-  }),
-);
+  p.setup = async () => {
+    img = await p.loadImage("cheezburger.jpg"); // https://share.google/Qg3HnLI440Cy0Q7ZV
+    p.createCanvas(237, 389);
+    p.image(img, 0, 0);
+    p.fill(255);
+    p.textFont("Impact");
+    p.textSize(29);
+    p.text("I CAN HAS", 60, 36);
+    p.text("CHEEZBURGER?", 30, 70);
+  };
+});
